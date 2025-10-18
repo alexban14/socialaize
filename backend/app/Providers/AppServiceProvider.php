278b1d13ebce57217bb60f1e2509ebfd978f15b8
Alex Ban <alexbanut10@gmail.com>
2025-10-18 20:35:06 +0300

    feat(backend): refactor interface concrete class registration

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,14 @@
 
 namespace App\Providers;
 
+use App\Repositories\User\UserRepository;
+use App\Repositories\User\UserRepositoryInterface;
+use App\Services\Auth\AuthServiceInterface;
+use App\Services\Auth\PassportAuthService;
+use App\Services\Auth\PasswordResetService;
+use App\Services\Auth\PasswordResetServiceInterface;
+use App\Services\User\UserService;
+use App\Services\User\UserServiceInterface;
 use Illuminate\Support\ServiceProvider;
 use Override;
 
@@ -13,7 +21,13 @@ class AppServiceProvider extends ServiceProvider
     #[Override]
     public function register(): void
     {
-        //
+        // services
+        $this->app->bind(AuthServiceInterface::class, PassportAuthService::class);
+        $this->app->bind(UserServiceInterface::class, UserService::class);
+        $this->app->bind(PasswordResetServiceInterface::class, PasswordResetService::class);
+
+        // repositories
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
     }
 
     /**
